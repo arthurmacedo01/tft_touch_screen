391c9a9ee9d1938cedf723dc026eae176a269357
Arthur_Macedo <arthurmacedo01@gmail.com>
2024-06-07 20:07:20 -0300

    first commit

--- README.md
+++ README.md
@@ -1,136 +1,64 @@
-# LVGL project for ESP32
+# LVGL ESP32 Button Control Demo
 
-This is an ESP32 demo project showcasing LVGL v7 with support for several display controllers and touch controllers.
-The demo application is the `lv_demo_widgets` project from the [lv_examples](https://github.com/lvgl/lv_examples) repository.
+This project demonstrates the use of LVGL (Light and Versatile Graphics Library) on an ESP32-based system. The application includes two buttons that control GPIO pins and reflect their states on the screen. The buttons are labeled "Compressor 1" and "Compressor 2", and change colors and text based on the states of the GPIO pins and connected sensors.
 
-- Version of ESP-IDF required 4.2. NOTE: We're trying to make this repo backwards compatible, usage of idf.py is encouraged.
-- Version of LVGL used: 7.9.
-- Version of lv_examples used: 7.9.
+## Features
+- Two buttons to control and monitor the state of GPIO pins.
+- Button states are visually represented with text and color changes.
+- Text labels "Compressor 1" and "Compressor 2" above the respective buttons.
 
-#### Table of content
-- [Get started](#get-started)
-- [Use LVGL in your ESP-IDF project](#use-lvgl-in-your-esp-idf-project)
-- [Use lvgl_esp32_drivers in your project](#use-lvgl_esp32_drivers-in-your-project)
-- [Platformio support](#platformio-support)
-- [ESP32-S2 Support](#esp32-s2-support)
+## Hardware Requirements
+- ESP32 development board
+- Touchscreen display compatible with LVGL
+- GPIO connections for sensors and outputs
 
-Example demo for TFT displays:
+## Software Requirements
+- ESP-IDF (Espressif IoT Development Framework)
+- LVGL (Light and Versatile Graphics Library)
 
-![Example GUI_DEMO with ESP32 using LVGL](images/new_photo.jpg)
+## Installation and Usage
 
-Monochrome support:
+### Hardware Setup
+1. Connect the touchscreen display to the ESP32.
+2. Connect the sensors to GPIO13 and GPIO12.
+3. Connect the output control to GPIO2 and GPIO26.
 
-![Example_monochrome demo with ESP32 using LVGL](images/new_mono.jpg)
+### Software Setup
+1. Clone this repository to your local machine:
+    ```bash
+    git clone https://github.com/yourusername/lvgl-esp32-button-demo.git
+    cd lvgl-esp32-button-demo
+    ```
+2. Configure the ESP-IDF environment:
+    ```bash
+    . $HOME/esp/esp-idf/export.sh
+    ```
+3. Build and flash the project:
+    ```bash
+    idf.py build
+    idf.py flash
+    ```
+4. Monitor the output:
+    ```bash
+    idf.py monitor
+    ```
 
-## Display and touch controllers
+## Code Explanation
+The `create_demo_application` function sets up the LVGL environment and creates two buttons with labels "Compressor 1" and "Compressor 2". The button states are updated based on the GPIO levels.
 
-The display and touch (indev) controllers are now into it's own repository, you can find it [here](https://github.com/lvgl/lvgl_esp32_drivers).
-To report any issue or add new display or touch (indev) drivers you can do so in the `lvgl_esp32_drivers` repo.
+### Button Event Handlers
+- `btn1_event_cb` and `btn2_event_cb` handle the click events for the buttons. They toggle the states of GPIO2 and GPIO26, respectively.
 
-## Get started
+### State Update
+- The `update_button_state` function checks the levels of GPIO13 and GPIO12 and updates the button labels and colors accordingly.
 
-### Prerequisites
+## License
+This project is licensed under the MIT License. See the LICENSE file for details.
 
-- ESP-IDF Framework.
+## Contributing
+Contributions are welcome! Please open an issue or submit a pull request for any changes or improvements.
 
-### Note
+## Acknowledgments
+- [LVGL](https://lvgl.io/) for the graphics library
+- [Espressif](https://www.espressif.com/) for the ESP32 platform
 
-This project tries to be compatible with both the ESP-IDF v3.x and v4.0, but using v4.0 is recommended.
-Instructions assume you are using the v4.x toolchain, otherwise use the make commands, e.g. instead of running `idf.py menuconfig`, run `make menuconfig`.
-
-### Build and run the demo.
-
-1. Clone this project by `git clone --recurse-submodules https://github.com/lvgl/lv_port_esp32.git`, this will pull this repo and its submodules.
-
-2. Get into the created `lv_port_esp32` directory.
-
-3. Run `idf.py menuconfig`
-
-4. Configure LVGL in `Components config->LVGL Configuration`. For monochrome displays use the mono theme and we suggest enabling the `unscii 8` font.
-
-5. Configure your display and/or touch controllers in `Components config->LVGL TFT Display Configuration` and `Components config->LVGL TOUCH Configuration`.
-
-6. Store your project configuration.
-
-7. Build the project with `idf.py build`
-
-8. If the build don't throw any errors, flash the demo with `idf.py -p (YOUR SERIAL PORT) flash` (with `make` this is just `make flash` - in 3.x PORT is configured in `menuconfig`)
-
-## Use LVGL in your ESP-IDF project
-
-LVGL now includes a Kconfig file which is used to configure most of the LVGL configuration options via menuconfig, so it's not necessary to use a custom `lv_conf.h` file.
-
-It is recommended to add LVGL as a submodule in your IDF project's git repo.
-
-From your project's root directory:
-1. Create a directory named `components` (if you don't have one already) with `mkdir -p components`.
-2. Clone the lvgl repository inside the `components` directory with `git submodule add https://github.com/lvgl/lvgl.git components/lvgl`
-3. Run `idf.py menuconfig`, go to `Component config` then `LVGL configuration` to configure LVGL.
-
-## Use lvgl_esp32_drivers in your project
-
-It is recommended to add [lvgl_esp32_drivers](https://github.com/lvgl/lvgl_esp32_drivers) as a submodule in your IDF project's git repo.
-
-From your project's root directory:
-1. Create a directory named `components` (if you don't have one already) with `mkdir -p components`.
-2. Clone the lvgl_esp32_drivers repository inside the `components` directory with `git submodule add https://github.com/lvgl/lvgl_esp32_drivers.git components/lvgl_esp32_drivers`
-3. Run `idf.py menuconfig`, go to `Component config` then `LVGL TFT configuration` and `LVGL TFT Display configuration` to configure lvgl_esp32_drivers.
-
-## Platformio support
-
-Using the [lv_platformio](https://github.com/lvgl/lv_platformio) project add the following lines to `platformio.ini` file:
-
-```
-[env:esp32]
-platform = espressif32
-framework = espidf
-board = esp-wrover-kit
-```
-
-Change the default environment to `default_envs = esp32`.
-
-Modify the `main.c` like this:
-
-```c
-#include "lvgl.h"
-
-// #include "driver.h"
-
-#include "demo.h"
-
-int app_main(void)
-{
-    lv_init();
-
-    /* Initialize your hardware. */
-    
-    /* hw_init(); */
-
-    demo_create();
-
-    /* Create the UI or start a task for it.
-     * In the end, don't forget to call `lv_task_handler` in a loop. */
-
-    /* hw_loop(); */
-
-    return 0;
-}
-```
-
-For more information see: [platformio with espidf framework compability](https://github.com/lvgl/lv_port_esp32/issues/168).
-
-# ESP32-S2 Support
-
-Support for ESP32-S2 variant is Work In Progress.
-Smaller displays (e.g. 320x240) work fine, but larger ones need testing.
-
-## Background
-
-ESP32-S2 has less on-chip SRAM than its predecessor ESP32 (520kB vs. 320kB).
-This causes problems with memory allocation with large LVGL display buffers as they don't fit into the on-chip memory
-and external PSRAM is not accessible by DMA.
-
-Moreover, static allocation to external PSRAM is not yet supported
-(see [GitHub issue](https://github.com/espressif/esp-idf/issues/6162)).
-
-At this momement, the buffers are dynamicaly allocated with DMA capabilty and memory allocator handles the rest.
--- main/main.c
+++ main/main.c
@@ -1,13 +1,3 @@
-/* LVGL Example project
- *
- * Basic project to test LVGL on ESP32 based projects.
- *
- * This example code is in the Public Domain (or CC0 licensed, at your option.)
- *
- * Unless required by applicable law or agreed to in writing, this
- * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.
- */
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,8 +10,8 @@
 #include "esp_system.h"
 #include "driver/gpio.h"
 #include "esp_timer.h"
+#include "esp_log.h"
 
-/* Littlevgl specific */
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
 #include "lvgl.h"
 #else
@@ -30,48 +20,63 @@
 
 #include "lvgl_helpers.h"
 
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+#if defined CONFIG_LV_USE_DEMO_WIDGETS
+#include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
+#elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
+#include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
+#elif defined CONFIG_LV_USE_DEMO_BENCHMARK
+#include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
+#elif defined CONFIG_LV_USE_DEMO_STRESS
+#include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
+#else
+#error "No demo application selected."
+#endif
+#endif
+
 #define TAG "demo"
 #define LV_TICK_PERIOD_MS 1
 
 static void lv_tick_task(void *arg);
 static void guiTask(void *pvParameter);
 static void create_demo_application(void);
-
-static lv_obj_t *btn1;
-static lv_obj_t *btn2;
-
-void app_main() {
-    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
-}
+static void btn1_event_cb(lv_obj_t *btn, lv_event_t event);
+static void btn2_event_cb(lv_obj_t *btn, lv_event_t event);
+static void update_button_state(void);
 
 SemaphoreHandle_t xGuiSemaphore;
+static int gpio2_state = 0;  // Variable to track GPIO2 state
+static int gpio26_state = 0; // Variable to track GPIO26 state
 
-static void guiTask(void *pvParameter) {
-    (void) pvParameter;
+void app_main()
+{
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096 * 2, NULL, 0, NULL, 1);
+}
+
+static void guiTask(void *pvParameter)
+{
+    (void)pvParameter;
     xGuiSemaphore = xSemaphoreCreateMutex();
 
     lv_init();
     lvgl_driver_init();
 
-    lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    lv_color_t *buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf1 != NULL);
 
-    #ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    lv_color_t* buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+    lv_color_t *buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf2 != NULL);
-    #else
+#else
     static lv_color_t *buf2 = NULL;
-    #endif
+#endif
 
     static lv_disp_buf_t disp_buf;
     uint32_t size_in_px = DISP_BUF_SIZE;
 
-    #if defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_IL3820 || \
-        defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_JD79653A || \
-        defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_UC8151D || \
-        defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_SSD1306
+#if defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_IL3820 || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_JD79653A || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_UC8151D || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_SSD1306
     size_in_px *= 8;
-    #endif
+#endif
 
     lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
 
@@ -79,89 +84,185 @@ static void guiTask(void *pvParameter) {
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = disp_driver_flush;
 
-    #if defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT || defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT_INVERTED
+#if defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT || defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT_INVERTED
     disp_drv.rotated = 1;
-    #endif
+#endif
 
-    #ifdef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+#ifdef CONFIG_LV_TFT_DISPLAY_MONOCHROME
     disp_drv.rounder_cb = disp_driver_rounder;
     disp_drv.set_px_cb = disp_driver_set_px;
-    #endif
+#endif
 
     disp_drv.buffer = &disp_buf;
     lv_disp_drv_register(&disp_drv);
 
-    #if CONFIG_LV_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
+#if CONFIG_LV_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
     lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.read_cb = touch_driver_read;
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     lv_indev_drv_register(&indev_drv);
-    #endif
+#endif
 
     const esp_timer_create_args_t periodic_timer_args = {
         .callback = &lv_tick_task,
-        .name = "periodic_gui"
-    };
+        .name = "periodic_gui"};
     esp_timer_handle_t periodic_timer;
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
-    create_demo_application();
-
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = (1ULL << GPIO_NUM_2) | (1ULL << GPIO_NUM_26);
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&io_conf);
+
+    io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = (1ULL << 26) | (1ULL << 27);
+    io_conf.pin_bit_mask = (1ULL << GPIO_NUM_13) | (1ULL << GPIO_NUM_12);
     io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
     io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
     gpio_config(&io_conf);
 
-    while (1) {
+    create_demo_application();
+
+    while (1)
+    {
         vTaskDelay(pdMS_TO_TICKS(10));
-        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
+
+        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY))
+        {
             lv_task_handler();
-
-            if (gpio_get_level(GPIO_NUM_26) == 1) {
-                lv_obj_set_style_local_bg_color(btn1, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_GREEN);
-            } else {
-                lv_obj_set_style_local_bg_color(btn1, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
-            }
-
-            if (gpio_get_level(GPIO_NUM_27) == 1) {
-                lv_obj_set_style_local_bg_color(btn2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_GREEN);
-            } else {
-                lv_obj_set_style_local_bg_color(btn2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
-            }
-
+            update_button_state();
             xSemaphoreGive(xGuiSemaphore);
         }
     }
 
     free(buf1);
-    #ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
     free(buf2);
-    #endif
+#endif
     vTaskDelete(NULL);
 }
 
-static void create_demo_application(void) {
-    lv_obj_t *scr = lv_disp_get_scr_act(NULL);
+static void btn1_event_cb(lv_obj_t *btn, lv_event_t event)
+{
+    if (event == LV_EVENT_CLICKED)
+    {
+        lv_obj_t *label = lv_obj_get_child(btn, NULL);
+        lv_label_set_text(label, "Acionando");
 
-    btn1 = lv_btn_create(scr, NULL);
-    lv_obj_set_size(btn1, 120, 50);
-    lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, -60);
-    lv_obj_t *label1 = lv_label_create(btn1, NULL);
-    lv_label_set_text(label1, "Bomba 1");
-
-    btn2 = lv_btn_create(scr, NULL);
-    lv_obj_set_size(btn2, 120, 50);
-    lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 60);
-    lv_obj_t *label2 = lv_label_create(btn2, NULL);
-    lv_label_set_text(label2, "Bomba 2");
+        gpio2_state = !gpio2_state;
+        gpio_set_level(GPIO_NUM_2, gpio2_state);
+        gpio26_state = 0;
+        gpio_set_level(GPIO_NUM_26, 0);
+    }
 }
 
-static void lv_tick_task(void *arg) {
-    (void) arg;
+static void btn2_event_cb(lv_obj_t *btn, lv_event_t event)
+{
+    if (event == LV_EVENT_CLICKED)
+    {
+        lv_obj_t *label = lv_obj_get_child(btn, NULL);
+        lv_label_set_text(label, "Acionando");
+
+        gpio26_state = !gpio26_state;
+        gpio_set_level(GPIO_NUM_26, gpio26_state);
+        gpio2_state = 0;
+        gpio_set_level(GPIO_NUM_2, 0);
+    }
+}
+
+static void update_button_state(void)
+{
+    lv_obj_t *scr = lv_disp_get_scr_act(NULL);
+    lv_obj_t *btn2 = lv_obj_get_child(scr, NULL);
+    lv_obj_t *label2 = lv_obj_get_child(btn2, NULL);
+    lv_obj_t *btn1 = lv_obj_get_child(scr, btn2);
+    lv_obj_t *label1 = lv_obj_get_child(btn1, NULL);
+
+    int gpio13_level = gpio_get_level(GPIO_NUM_13);
+    int gpio12_level = gpio_get_level(GPIO_NUM_12);
+
+    ESP_LOGI(TAG, "GPIO2 state: %d, GPIO13 level: %d", gpio2_state, gpio13_level);
+    ESP_LOGI(TAG, "GPIO26 state: %d, GPIO12 level: %d", gpio26_state, gpio12_level);
+
+    if (gpio2_state == 0 && gpio13_level == 0)
+    {
+        lv_label_set_text(label1, "Comp 1 Desligado");
+        lv_obj_set_style_local_bg_color(btn1, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
+    }
+    else if (gpio2_state == 0 && gpio13_level == 1)
+    {
+        lv_label_set_text(label1, "Comp 1 Desligando");
+        lv_obj_set_style_local_bg_color(btn1, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_YELLOW);
+    }
+    else if (gpio2_state == 1 && gpio13_level == 0)
+    {
+        lv_label_set_text(label1, "Comp 1 Ligando");
+        lv_obj_set_style_local_bg_color(btn1, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_YELLOW);
+    }
+    else if (gpio2_state == 1 && gpio13_level == 1)
+    {
+        lv_label_set_text(label1, "Comp 1 Ligado");
+        lv_obj_set_style_local_bg_color(btn1, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_GREEN);
+    }
+
+    if (gpio26_state == 0 && gpio12_level == 0)
+    {
+        lv_label_set_text(label2, "Comp 2 Desligado");
+        lv_obj_set_style_local_bg_color(btn2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
+    }
+    else if (gpio26_state == 0 && gpio12_level == 1)
+    {
+        lv_label_set_text(label2, "Comp 2 Desligando");
+        lv_obj_set_style_local_bg_color(btn2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_YELLOW);
+    }
+    else if (gpio26_state == 1 && gpio12_level == 0)
+    {
+        lv_label_set_text(label2, "Comp 2 Ligando");
+        lv_obj_set_style_local_bg_color(btn2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_YELLOW);
+    }
+    else if (gpio26_state == 1 && gpio12_level == 1)
+    {
+        lv_label_set_text(label2, "Comp 2 Ligado");
+        lv_obj_set_style_local_bg_color(btn2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_GREEN);
+    }
+}
+
+static void create_demo_application(void)
+{
+    lv_obj_t *scr = lv_disp_get_scr_act(NULL);
+    lv_coord_t screen_width = lv_disp_get_hor_res(NULL);
+    lv_coord_t screen_height = lv_disp_get_ver_res(NULL);
+    lv_coord_t btn_width = screen_width * 0.4;
+    lv_coord_t btn_height = screen_height * 0.4;
+
+    lv_obj_t *btn1 = lv_btn_create(scr, NULL);
+    lv_obj_set_pos(btn1, (screen_width - btn_width) / 2 - btn_width / 2 - 10, (screen_height - btn_height) / 2);
+    lv_obj_set_size(btn1, btn_width, btn_height);
+    lv_obj_set_event_cb(btn1, btn1_event_cb);
+
+    lv_obj_t *label1 = lv_label_create(btn1, NULL);
+    lv_label_set_text(label1, "Desligado");
+    lv_obj_align(label1, btn1, LV_ALIGN_CENTER, 0, 0);
+
+    lv_obj_t *btn2 = lv_btn_create(scr, NULL);                                                               // Create btn2 independently
+    lv_obj_set_pos(btn2, (screen_width - btn_width) / 2 + btn_width / 2 + 10, (screen_height - btn_height) / 2); // Position btn2 on the opposite side of the screen
+    lv_obj_set_size(btn2, btn_width, btn_height);
+    lv_obj_set_event_cb(btn2, btn2_event_cb);
+
+    lv_obj_t *label2 = lv_label_create(btn2, NULL);
+    lv_label_set_text(label2, "Desligado");
+    lv_obj_align(label2, btn2, LV_ALIGN_CENTER, 0, 0);
+
+    update_button_state();
+}
+
+static void lv_tick_task(void *arg)
+{
+    (void)arg;
     lv_tick_inc(LV_TICK_PERIOD_MS);
 }
